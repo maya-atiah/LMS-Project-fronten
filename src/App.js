d5421ff716a365a34c627e1f42a0e24b845d0e81
Assala <ismailassala1@gmail.com>
2023-03-05 17:27:32 +0200

    working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
-import Navbar from "./components/Navbar";
+import { React, useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {Navigate } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
 import Home from "./components/Home/Home";
 import Classes from "./components/Classes/classes";
 import Courses from "./components/Courses/Courses";
@@ -10,21 +12,29 @@ import Reports from "./components/Reports/Reports";
 import Header from "./components/Header/Header";
 // import Calender from "./components/Calender/Calender";
 import "./style/index.css";
-
+import LoginPage from "./components/Login/Login";
 
 function App() {
-  const [navVisible, showNavbar] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(
+    window.localStorage.getItem("loggedIn")
+  );
+
+  useEffect(() => {
+    setIsLoggedIn(window.localStorage.getItem("loggedIn"));
+  }, []);
 
   return (
     
     <BrowserRouter>
       <div className="App">
+        
       
-        <Navbar visible={navVisible} show={showNavbar} />
-        <Header />    
-        {/* <Calender/>*/}
+        {/* <Navbar visible={navVisible} show={showNavbar} />
+        <Header />     */}
+        {/* <Calender/>
 
-        <Routes>
+        {/* <Routes>
+          <Route path="/login" element={isLoggedIn === "true" ? <Home/> : <Login />} />
           <Route path="/Home" element={<Home />} />
           <Route path="/Classes" element={<Classes />} />
           <Route path="/Courses" element={<Courses />} />
@@ -32,7 +42,10 @@ function App() {
           <Route path="/Attendance" element={<Attendance />} />
           <Route path="/Reports" element={<Reports />} />
          
-        </Routes>
+        </Routes> */}
+
+        <LoginPage/>
+
       </div>
     </BrowserRouter>
   );
