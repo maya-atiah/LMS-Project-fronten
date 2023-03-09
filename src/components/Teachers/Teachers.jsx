24b97f31e10f756df5652f0fda5588cdf2444fcc
omar kouzi <omarkouzi81248@gmail.com>
2023-03-09 10:10:13 +0000

    teacher-delete&-teacher-add-style

--- src/components/Teachers/Teachers.jsx
+++ src/components/Teachers/Teachers.jsx
@@ -1,14 +1,25 @@
 import { React } from "react";
-import { useState, useEffect } from "react";
 import "./Teacher.css";
 import axios from "axios";
 import { TeacherCard } from "./TeacherCard/TeacherCard.jsx";
 import Navhead from "../../components/Navhead";
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
+import PopupTeacher from "./PopupTeacher";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import Box from "@mui/material/Box";
+import { Typography } from "@mui/material";
 
 function Teachers() {
   const [teacher, setTeacher] = useState([]);
+  const [buttonPopup, setButtonPopup] = useState(false);
+  const [firstName, setFname] = useState("");
+  const [lastName, setLname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phoneNumber, setPhone] = useState("");
+  const [role, setRole] = useState("");
+  const [isPending, setIsPending] = useState(false);
 
   const navigate = useNavigate();
   useEffect(() => {
@@ -16,8 +27,6 @@ function Teachers() {
       navigate("/");
     }
   }, []);
- 
-
 
   const getAllTeachers = () =>
     axios
@@ -39,29 +48,156 @@ function Teachers() {
     );
   });
   const deleteTeacher = async (id) => {
-    await axios.delete(`http://localhost:8000/api/teacher/${id}`);
-   getAllTeachers();
- };
+    await axios.delete(`http://localhost:8000/api/user/${id}`);
+    getAllTeachers();
+  };
+
+  const addTeacher = () => {
+    axios
+      .post("http://localhost:8000/api/user", {
+        firstName,
+        lastName,
+        email,
+        password,
+        phoneNumber,
+        role
+      })
+      .then(() => {});
+    setButtonPopup(false);
+  };
+
   useEffect(() => {
     getAllTeachers();
-  }, []);
+  }, [buttonPopup]);
+  const submitHandler = (e) => {
+    e.preventDefault();
+    addTeacher();
+    // setButtonPopup(false);
+  };
 
   return (
     <>
       <Navhead />
 
-      <div className='Teachercontainer'>
-        <div className='allTeachersSection'>
-          <p className='allTeachersTitle'>All Teachers</p>
-          <div className='Teachersline'></div>
-          <div className='teacherCardsContainer'>{teacherCard}</div>
+      <div className="Teachercontainer">
+        <div className="allTeachersSection">
+          <div className="titleTeacherAdd" onClick={() => setButtonPopup(true)}>
+            <h3 className="allTeachersTitle">All Teachers</h3>
+            <div className="addTeacher">
+              <div className="alignAddTeacher">
+                <AddCircleIcon /> <h3>Add Teacher</h3>
+              </div>
+
+              <PopupTeacher
+                trigger={buttonPopup}
+                setTrigger={() => setButtonPopup(false)}
+              >
+                <Box
+                  component="form"
+                  sx={{
+                    "& > :not(style)": { m: 1 },
+                  }}
+                  noValidate
+                  autoComplete="off"
+                >
+                  <Typography
+                    gutterBottom
+                    color="white"
+                    variant="h4"
+                    component="div"
+                  >
+                    Add Teacher
+                  </Typography>
+                  <div className="input">
+                    <div className="input-label-flex">
+                      <label htmlFor="firstName">First Name</label>
+                      <input
+                        type="text"
+                        id="firstName"
+                        name="firstName"
+                        onChange={(e) => setFname(e.target.value)}
+                      />
+                    </div>
+                    <div className="input-label-flex">
+                      {" "}
+                      <label htmlFor="lastName">Last Name</label>
+                      <input
+                        type="text"
+                        id="lastName"
+                        name="lastName"
+                        onChange={(e) => setLname(e.target.value)}
+                      />
+                    </div>
+                    <div className="input-label-flex">
+                      {" "}
+                      <label htmlFor="Email">Email</label>
+                      <input
+                        type="mail"
+                        id="email"
+                        name="email"
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
+                    </div>
+                    <div className="input-label-flex">
+                      {" "}
+                      <label htmlFor="Password">Password</label>
+                      <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                      />
+                    </div>
+                    <div className="input-label-flex">
+                      {" "}
+                      <label htmlFor="phoneNumber">Phone Number</label>
+                      <input
+                        type="text"
+                        id="phoneNumber"
+                        name="phoneNumber"
+                        onChange={(e) => setPhone(e.target.value)}
+                      />
+                    </div>{" "}
+                    <div className="input-label-flex">
+                      {" "}
+                      <label htmlFor="role">role</label>
+                      <input
+                        type="text"
+                        id="role"
+                        name="role"
+                        onChange={(e) => setRole(e.target.value)}
+                      />
+                    </div>
+                  </div>
+
+                  {!isPending && (
+                    <button className="btn-add-teacher" onClick={submitHandler}>
+                      add
+                    </button>
+                  )}
+                  {isPending && (
+                    <button className="btn-add-teacher" onClick={submitHandler}>
+                      adding course
+                    </button>
+                  )}
+                </Box>
+              </PopupTeacher>
+            </div>
+          </div>
+          <div className="Teachersline"></div>
+          <div className="teacherCardsContainer">
+            {teacher.map((each, key) => (
+              <TeacherCard
+                key={key}
+                teacher={each}
+                deleteTeacher={deleteTeacher}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </>
   );
-
-
-
 }
 
 export default Teachers;
