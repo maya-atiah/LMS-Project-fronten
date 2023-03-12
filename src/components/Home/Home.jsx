61f4ac8e82fe7cb0e648e868e2520c75dc301386
maya-atiah <119121545+maya-atiah@users.noreply.github.com>
2023-03-12 22:54:25 +0200

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -82,7 +82,7 @@ function Home() {
   return (
     <div>
       <Navhead />
-      <section classNAme="home-section>
+      <section classNAme="home-section">
         <div className="component-container">
           <h1 className="home"> Home </h1>
           <div className="cards">
