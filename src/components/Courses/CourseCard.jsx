45074a504f1b149f569d18f38fc93f1fd9f83bd9
Maya <maya.atiah.99@outlook.com>
2023-03-15 21:21:46 +0200

    changing in teachers side

--- src/components/Courses/CourseCard.jsx
+++ src/components/Courses/CourseCard.jsx
@@ -12,28 +12,9 @@ import {
 import "../Courses/CourseCard.css";
 import { DeleteForeverRounded } from "@mui/icons-material";
 
-export default function CourseCard({ course ,handleDelete}) {
-  
-
+export default function CourseCard({ course, handleDelete }) {
   return (
     <div>
-      {/* <Card elevation={1}>
-          <CardHeader
-            action={
-              <IconButton>
-                <DeleteOutlined />
-              </IconButton>
-            }
-            title='Subject'
-            subheader={course.subject}
-          />
-          <CardContent>
-            <Typography variant="body2" color="textSecondary">
-             Hi
-            </Typography>
-          </CardContent>
-        </Card> */}
-
       <Card className='course-CardColor' sx={{ maxWidth: 250 }}>
         <CardActionArea>
           <CardMedia
@@ -43,22 +24,26 @@ export default function CourseCard({ course ,handleDelete}) {
             alt='course'
           />
           <CardContent>
-            <Typography gutterBottom color='white'variant='h5' component='div'>
+            <Typography gutterBottom color='white' variant='h5' component='div'>
               {course.subject}
             </Typography>
-           <p className="course-para"><strong>Courses are given by Teachers:</strong> </p> 
+            <p className='course-para'>
+              <strong>Courses are given by Teachers:</strong>{" "}
+            </p>
             {course.teachers.map((x) => {
               return (
                 <Typography variant='body1' color='white' key={x.id}>
-                
-                 {x.firstName} {x.lastName} 
+                  {x.firstName} {x.lastName}
                 </Typography>
               );
             })}
           </CardContent>
         </CardActionArea>
-        <CardActions >
-          <IconButton aria-label='delete' onClick={()=>handleDelete(course.id)} >
+        <CardActions>
+          <IconButton
+            aria-label='delete'
+            onClick={() => handleDelete(course.id)}
+          >
             <DeleteForeverRounded />
           </IconButton>
         </CardActions>
